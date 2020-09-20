fb8629ce56d1c2fb8c32c2617058a11b791f4cd4
Dylan Clements <dclements@protonmail.com>
2020-09-20 16:02:31 -0400

    Initial JS

---
+++ src/index.js
@@ -0,0 +1,18 @@
+// import "./styles.css";
+
+const inputField = document.querySelector('#input');
+// const inputButton = document.querySelector('#inputButton')
+// const input = document.querySelector("input");
+// console.log("First"+input.value)
+// input.addEventListener("click", getInput);
+
+
+inputButton.addEventListener('click', function() {
+    console.log(inputField.value);
+  });
+
+// function getInput(){
+//     let inputValue = document.getElementById("input").value;
+
+//     console.log(inputValue) 
+// }
