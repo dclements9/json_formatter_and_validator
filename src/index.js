d35ac1cb7a005664d9bbdde162344d5833f5422c
dclements9 <dclements@protonmail.com>
2020-10-01 13:21:57 -0400

    try/Catch & placeholder - sanitizeError()

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
-// import "./styles.css";
-
 const inputField = document.querySelector('#input');
 
 inputButton.addEventListener('click', function() {
     // Input Field = inputfield.value
-
     console.log(inputField.value);
+
+    //let result = JSON.parse(inputField.value)
+    let result = ''
+
+    try {
+      result = JSON.parse(inputField.value)
+    }catch(error){
+      if (error){
+        result = sanitizeError(error);
+      }
+    }
+    //Outputs result to result Div on page
+      document.getElementById("resultDiv").innerHTML = result
+
   });
 
-// function getInput(){
-//     let inputValue = document.getElementById("input").value;
+  function sanitizeError(error){
 
-//     console.log(inputValue) 
-// }
+    const sanitizedResult = "Sanitized this:" + error;
+    return sanitizedResult;
+  }
