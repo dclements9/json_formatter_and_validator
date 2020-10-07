ed2152164950ef0fedc0f3e00c335a0314aef734
Dylan Clements <dclements@protonmail.com>
2020-10-07 09:33:08 -0400

    JS resultDiv -> resultPre

--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@
 const inputField = document.querySelector('#input');
 
 inputButton.addEventListener('click', function() {
-    // Input Field = inputfield.value
     let result = ''
-
     // Testing -  {"foo":1,"bar":2,"baz":[4,5,6]}
     try {
       if (JSON.parse(inputField.value)){      
@@ -20,14 +18,14 @@ inputButton.addEventListener('click', function() {
         result = sanitizeError(error);
       }
     }
-    //Outputs result to result Div on page
-      document.getElementById("resultDiv").innerHTML = result;
+    //Outputs result to resultPre on page (Pre = Preformatted)
+      document.getElementById("resultPre").innerHTML = result;
   });
 
   function sanitizeError(error){
     const sanitizedResult = "Sanitized this:" + error;
 //TODO
-    const failedGif = document.getElementById("resultDiv").innerHTML = 
+    const failedGif = document.getElementById("resultPre").innerHTML = 
     '<img src="https://media.giphy.com/media/HNEmXQz7A0lDq/giphy.gif"/>';
     //return sanitizedResult;
     return failedGif;
