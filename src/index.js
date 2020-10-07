8704b20b06c77da1ffd0cb8f5730b99f3ed45b8e
Dylan Clements <dclements@protonmail.com>
2020-10-07 10:00:06 -0400

    Display error and code cleanup

--- src/index.js
+++ src/index.js
@@ -1,20 +1,15 @@
-// TODO: 1. Failed = Clarify errors to display for user.
-//       2. Success = Pretty Print JSON 
-
 const inputField = document.querySelector('#input');
 
 inputButton.addEventListener('click', function() {
     let result = ''
-    // Testing -  {"foo":1,"bar":2,"baz":[4,5,6]}
+
     try {
       if (JSON.parse(inputField.value)){      
       let jsonPrint = JSON.stringify(JSON.parse(inputField.value),null,2); 
-      //document.getElementById("resultDiv").innerHTML = 
-      result = jsonPrint;
+        result = jsonPrint;
       }
     }catch(error){
       if (error){
-        console.log(error)
         result = sanitizeError(error);
       }
     }
@@ -23,10 +18,8 @@ inputButton.addEventListener('click', function() {
   });
 
   function sanitizeError(error){
-    const sanitizedResult = "Sanitized this:" + error;
-//TODO
     const failedGif = document.getElementById("resultPre").innerHTML = 
-    '<img src="https://media.giphy.com/media/HNEmXQz7A0lDq/giphy.gif"/>';
-    //return sanitizedResult;
+    error + '</br>' + '</br>' +'<img src="https://media.giphy.com/media/HNEmXQz7A0lDq/giphy.gif"/>';
+
     return failedGif;
   }
